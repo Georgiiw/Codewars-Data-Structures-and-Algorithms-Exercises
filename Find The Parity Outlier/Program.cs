32182b829155ac342c534ec4c40e0a34b88576e9
Georgi <Georgiivanover@gmail.com>
2024-05-02 22:35:39 +0300

    Find the parity outlier

---
+++ Find The Parity Outlier/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Find_The_Parity_Outlier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] integers = new int[] { 1, 15, 3, 5, 7, 9, 12 };
+            Console.WriteLine(Find(integers));
+        }
+
+        public static int Find(int[] integers)
+        {
+           int count = integers.Count(n => n % 2 == 0);
+
+            return count > 1 ? integers.First(n => n % 2 == 1) : integers.First(n => n % 2 == 0);
+        }
+    }
+}
