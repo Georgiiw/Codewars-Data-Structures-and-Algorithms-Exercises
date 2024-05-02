ac65b4d876f8e92b836394c14a46a8cf15d9a6e4
Georgi <Georgiivanover@gmail.com>
2024-05-02 21:42:20 +0300

    How many numbers?

---
+++ How Many Numbers/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace How_Many_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SelNumber(100, 3));
+            
+        }
+        public static int SelNumber(int number, int excessor)
+        {
+            int counter = 0;
+
+            //Logic
+            for (int i = 0; i <= number; i++)
+            {
+                bool pass = false;            
+                string[] num = i.ToString().Select(d => d.ToString()).ToArray();
+                for (int j = 0; j < num.Length - 1; j++)
+                {
+                    int currNum = int.Parse(num[j]);
+                    int NextNum = int.Parse(num[j + 1]);
+                    if (currNum == NextNum || currNum > NextNum || NextNum - currNum > excessor)
+                    {
+                        pass = false;
+                        break;
+                    }
+                    if (NextNum - currNum <= excessor)
+                    {
+                        pass = true;
+                    }
+                }
+                if (pass)
+                {
+                    counter++;
+                }
+
+            }
+            return counter;
+        }
+    }
+}
