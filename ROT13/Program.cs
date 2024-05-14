ae37f45b84be2a638a5d02962c0de40e1826dcb4
Georgi <Georgiivanover@gmail.com>
2024-05-14 13:34:20 +0300

    ROT13 translate

---
+++ ROT13/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+namespace ROT13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Rot13("I love you."));
+        }
+
+        public static string Rot13(string input)
+        {
+            StringBuilder sb = new StringBuilder();
+            List<char> list = new List<char>("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz");
+            List<char> rot13 = new List<char>("NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm");
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (Char.IsLetter(input[i]))
+                {
+                    int num = list.IndexOf(input[i]);
+                    sb.Append(rot13[num]);                 
+                }
+                else
+                {
+                    sb.Append(input[i]);
+                }
+            }
+            return sb.ToString();
+        }
+    }
+}
