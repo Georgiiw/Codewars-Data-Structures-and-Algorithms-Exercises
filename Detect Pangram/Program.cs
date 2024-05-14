b136cc5f903b3eb4db5dc6a41566e6486b7465ed
Georgi <Georgiivanover@gmail.com>
2024-05-14 12:29:53 +0300

    Detect Pangram

---
+++ Detect Pangram/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Detect_Pangram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsPangram("abcdefghywyreyijkltretetmnopqrfdsfstuvwxyzxzxzx"));
+           
+        }
+        public static bool IsPangram(string str)
+        {
+            List<char> list = new List<char>("abcdefghijklmnopqrstuvwxyz");
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (str.ToCharArray().Any(x => x == list[i]))
+                {
+                    list.Remove(list[i]);
+                    i--;
+                }
+            }
+            return list.Count == 0;
+        }
+    }
+}
