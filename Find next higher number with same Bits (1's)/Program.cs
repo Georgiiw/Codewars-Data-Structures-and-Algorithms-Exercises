724061b40295c4a087da9f2fa54b222974ab974b
Georgi <Georgiivanover@gmail.com>
2024-05-29 17:19:49 +0300

    Find next higher number with same Bits (1's)

---
+++ Find next higher number with same Bits (1's)/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Find_next_higher_number_with_same_Bits__1_s_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(NextHigher(128));
+        }
+
+        public static int NextHigher(int value)
+        {
+            int next = value + (value & -value);
+            int rightOnesPattern = value ^ next;
+            rightOnesPattern = (rightOnesPattern >> 2) / (value & -value);
+            return next | rightOnesPattern;
+        }     
+    }
+}
