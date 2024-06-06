7bd2b604475294ae0c15fc1b36c11513c2dab822
Georgi <Georgiivanover@gmail.com>
2024-06-06 16:42:44 +0300

    Int32 to IPv4

---
+++ Int32 to IPv4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Net;
+
+namespace Int32_to_IPv4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(UInt32ToIP(2154959208));
+        }
+        public static string UInt32ToIP(uint ip)
+        {         
+            return $"{IPAddress.Parse($"{ip}")}";
+        }
+    }
+}
